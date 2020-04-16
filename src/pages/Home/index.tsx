08c8bacb132f4abc5c671d5f330801923255dd48
Kaique Sacchi <sacchikaique@gmail.com>
2020-04-16 20:31:23 -0300

    Added (Home Page) Main interactions.
    Now user is able to:
    - Generate new random arrays
    - Run the default sorting algorithm

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, Text, View } from 'react-native';
+import quicksort from '../../sortingAlgorithms/quicksort';
 export default function Home() {
-  const list = [0.3, 0.8, 0.6, 0.4, 0.8, 0.6, 0.2, 0.5, 0.7, 0.4, 0.8, 0.4, 0.8];
+  const [list, setList] = useState<number[]>([]);
+  const [listSize, setListSize] = useState(20);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    generateRandomArray();
+  }, []);
+
+  function play() {
+    const swaps = quicksort([...list]);
+    if (swaps.length !== 0) setIsRunning(true);
+    let newList = [...list];
+    swaps.forEach((swap, index) => {
+      setTimeout(() => {
+        let aux = newList[swap[0]];
+        newList[swap[0]] = newList[swap[1]];
+        newList[swap[1]] = aux;
+        setList([...newList]);
+        if (index === swaps.length - 1) setIsRunning(false);
+      }, 100 * (index + 1));
+    });
+  }
+
+  function generateRandomArray(): number[] {
+    let array: number[] = [];
+    for (let i = 0; i < listSize; i++) {
+      array.push(Math.random());
+    }
+    setList(array);
+    return array;
+  }
+
   return (
     <View
       style={{
@@ -25,6 +56,63 @@ export default function Home() {
           />
         );
       })}
+      <TouchableOpacity
+        onPress={play}
+        disabled={isRunning}
+        accessibilityLabel="Run the sorting algorithm"
+        style={{
+          backgroundColor: '#8020f0',
+          height: 65,
+          width: 65,
+          borderRadius: 65,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          position: 'absolute',
+          right: 20,
+          bottom: 20
+        }}
+      >
+        <Text>></Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {}}
+        disabled={isRunning}
+        accessibilityLabel="Run the sorting algorithm"
+        style={{
+          backgroundColor: '#8020f0',
+          height: 65,
+          width: 65,
+          borderRadius: 65,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          position: 'absolute',
+          right: 100,
+          bottom: 20
+        }}
+      >
+        <Text>*</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={generateRandomArray}
+        disabled={isRunning}
+        accessibilityLabel="Run the sorting algorithm"
+        style={{
+          backgroundColor: '#8020f0',
+          height: 65,
+          width: 65,
+          borderRadius: 65,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          position: 'absolute',
+          right: 180,
+          bottom: 20
+        }}
+      >
+        <Text>?</Text>
+      </TouchableOpacity>
     </View>
   );
 }
