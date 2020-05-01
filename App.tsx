8ba3cbe6eb7384bc28c18bf8a8fe07701543d303
Kaique <sacchikaique@gmail.com>
2020-05-01 20:19:18 -0300

    Added (Icon and Splash screen)

--- App.tsx
+++ App.tsx
@@ -9,22 +9,3 @@ export default function App() {
     </NativeRouter>
   );
 }
-
-/*
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-*/
